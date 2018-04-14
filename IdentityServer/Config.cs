d402468324b20d0ad8d40ef46095786ffba66b04
Manuel Naujoks <mnaujoks@gmail.com>
2018-04-14 22:55:22 +0200

    IdentityServer UI

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer
+{
+	public static class Config
+	{
+		#region Scopes = Resources (Identity Resources und API Resources)
+		public static IEnumerable<IdentityResource> GetIdentityResources()
+		{
+			return new IdentityResource[]
+			{
+				new IdentityResources.OpenId(),
+				new IdentityResources.Profile(),
+			};
+		}
+
+		public static IEnumerable<ApiResource> GetApis()
+		{
+			return new ApiResource[]
+			{
+				new ApiResource("api", "Some API"),
+				//new ApiResource
+				//{
+				//	Name = "complicated_api",
+				//	DisplayName = "Complicated API",
+				//	UserClaims = { "name", "email"},
+				//	Scopes =
+				//	{
+				//		new Scope("full_access")
+				//		{
+				//			UserClaims = { "role"}
+				//		},
+				//		new Scope("read_only")
+				//	}
+				//}
+			};
+		}
+		#endregion
+
+		public static IEnumerable<Client> GetClients()
+		{
+			return new Client[]
+			{
+				new Client
+				{
+					ClientId = "console",
+					ClientName = "Console App",
+					AllowedGrantTypes = GrantTypes.ClientCredentials,
+					ClientSecrets = { new Secret("secret".Sha256()) },
+					AllowedScopes = { "api" }
+				},
+				new Client
+				{
+					ClientId = "spa",
+					ClientName = "SPA App",
+					AllowedGrantTypes = GrantTypes.Implicit,
+					RedirectUris = new List<string>
+					{
+						"http://localhost/signin-oidc"
+					},
+					PostLogoutRedirectUris = new List<string>
+					{
+						"http://localhost/signout-oidc-callback"
+					},
+					LogoUri = "http://localhost/signout-oidc",
+					AllowedScopes = { "openid", "profile", "api" }
+				},
+			};
+		}
+
+		public static List<TestUser> GetTestUsers()
+		{
+			return new List<TestUser>
+			{
+				new TestUser
+				{
+					SubjectId = "123", Username = "bob", Password = "bob",
+					Claims = new List<Claim>
+					{
+						new Claim("name", "Bob Loblaw"),
+						new Claim("email", "Bob@Loblaw.com"),
+						new Claim("location", "USA"),
+					}
+				}
+			};
+		}
+	}
+}
--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using IdentityServer4.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -23,10 +21,13 @@ namespace IdentityServer
 
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddMvc();
+
 			services.AddIdentityServer()
 				.AddDeveloperSigningCredential()
 				.AddInMemoryApiResources(Config.GetApis())
-				.AddInMemoryClients(Config.GetClients());
+				.AddInMemoryClients(Config.GetClients())
+				.AddTestUsers(Config.GetTestUsers());
 		}
 
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -38,53 +39,9 @@ namespace IdentityServer
 			{
 				app.UseDeveloperExceptionPage();
 			}
-
+			app.UseStaticFiles(); // Install IdentityServer UI: iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/IdentityServer/IdentityServer4.Quickstart.UI/release/get.ps1'))
 			app.UseIdentityServer();
-
-			app.Run(async (context) =>
-			{
-				await context.Response.WriteAsync("This is IdentityServer4!");
-			});
-		}
-	}
-
-	public static class Config
-	{
-		public static IEnumerable<ApiResource> GetApis()
-		{
-			return new ApiResource[]
-			{
-				new ApiResource("api", "Some API"),
-				//new ApiResource
-				//{
-				//	Name = "complicated_api",
-				//	DisplayName = "Complicated API",
-				//	UserClaims = { "name", "email"},
-				//	Scopes =
-				//	{
-				//		new Scope("full_access")
-				//		{
-				//			UserClaims = { "role"}
-				//		},
-				//		new Scope("read_only")
-				//	}
-				//}
-			};
-		}
-
-		public static IEnumerable<Client> GetClients()
-		{
-			return new Client[]
-			{
-				new Client
-				{
-					ClientId = "console",
-					ClientName = "Console App",
-					AllowedGrantTypes = GrantTypes.ClientCredentials,
-					ClientSecrets = { new Secret("secret".Sha256()) },
-					AllowedScopes = { "api" }
-				}
-			};
+			app.UseMvcWithDefaultRoute();
 		}
 	}
 }
